82da10d736ad39651730970687beda92248477c5
Eloy <eloymjunior00@gmail.com>
2023-10-20 16:41:29 -0300

    Alterações gerais

--- src/models/Brand.js
+++ src/models/Brand.js
@@ -27,7 +27,7 @@ class Brand extends Model {
   }
 
   static associate(models) {
-    this.belongsTo(models.Devices, {
+    this.hasMany(models.Devices, {
       foreignKey: "bl_uuid",
       as: "devices",
     });
