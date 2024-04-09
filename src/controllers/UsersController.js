df1344b8ec9e3cde887c5ff8fcbe55451f60e2d6
Eloy <eloymjunior00@gmail.com>
2024-04-09 10:31:13 -0300

    Alterações

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -3300,13 +3300,13 @@ const usersController = new UsersController();
 // usersController.agendarreinicioDispositivos();
 
 //Envio relatorio de dispositivos acima e abaixo do estimado
-// usersController.agendarmonitorGeração();
+usersController.agendarmonitorGeração();
 
 //Cron para  envio de alerta quando a geração real estiver x% abaixo da geração estimada
-// usersController.agendarAlertasGeracao();
+usersController.agendarAlertasGeracao();
 
 //Envio alertas da tabela alerts do banco
-// usersController.agendarVerificacaoDeAlertas();
+usersController.agendarVerificacaoDeAlertas();
 
 //Envio automatico do 'envio massivo de relatorios'
 // usersController.agendarenvioEmailRelatorio()
