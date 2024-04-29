c2e440a7c377afebbecf7efa75dba1e544e2d4ab
Eloy <eloymjunior00@gmail.com>
2024-04-29 16:04:54 -0300

    General changes 2

--- src/server.js
+++ src/server.js
@@ -6,13 +6,10 @@ import express from "express";
 import cors from "cors";
 import routes from "./routes";
 import "moment/locale/pt-br";
-import "./massive-email/verifiy_massive_emails.js";
 require("./database");
 
 const PORT = 8080;
-// const HOST = "localhost";
 const HOST = "0.0.0.0";
-// const HOST = "104.131.163.240";
 
 const app = express();
 
@@ -25,13 +22,11 @@ app.use((req, res, next) => {
   }
 });
 
-const corsOptions = {
-  origin: "http://localhost:3000",
-};
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(routes);
 
 app.listen(PORT, HOST);
+
 app.get("/", function (req, res, next) {
   res.send("Olá!");
 });
