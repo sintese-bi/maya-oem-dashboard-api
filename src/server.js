4db7103d43ead1f0eb37019a8e3af8fbac785964
felipe-gl18 <felipegadelha2004@gmail.com>
2024-05-10 15:02:39 -0300

    adds

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ import "./massive-email/verifiy_massive_emails.js";
 import { WebSocketService } from "./service/websocket.js";
 require("./database");
 
-const PORT = 8082;
+const PORT = 8080;
 const HOST = "0.0.0.0";
 
 const app = express();
